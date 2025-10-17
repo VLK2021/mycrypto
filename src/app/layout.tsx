276eeea5b3399248af2587c6bcc633026cb26024
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-10-17 16:00:02 +0300

    add change theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 
 import "./globals.css";
-import {LanguageProvider} from "@/context";
+import {LanguageProvider, ThemeProvider} from "@/context";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -29,9 +29,11 @@ export default function RootLayout({
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        <LanguageProvider>
-            {children}
-        </LanguageProvider>
+        <ThemeProvider>
+            <LanguageProvider>
+                {children}
+            </LanguageProvider>
+        </ThemeProvider>
         </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import LangSwitcher from "@/components/LangSwitcher";
 import Sidebar from "@/components/Sidebar";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+import LangSwitcher from "@/components/LangSwitcher";
 
 export default function Home() {
-  return (
-    <div>
-      APP
-        <LangSwitcher/>
-
-        <Sidebar/>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-background text-text">
+            <div className="bg-card border border-border">
+                APP
+                <div className="mt-4 flex gap-3">
+                    <LangSwitcher />
+                    <ThemeSwitcher />
+                </div>
+                <div className="mt-6">
+                    <Sidebar />
+                </div>
+            </div>
+        </div>
+    );
 }
