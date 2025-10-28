0b12a3305e77bd55ef0f8c7f5620aa347cdf5a9d
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-10-28 10:12:17 +0200

    commit

--- src/components/dashboard/TopStats.tsx
+++ src/components/dashboard/TopStats.tsx
@@ -2,31 +2,50 @@
 
 import React from "react";
 import CardContainer from "@/components/elements/CardContainer";
-import {PortfolioExposure, PortfolioStructure, RealPnl, TotalBalance, TotalInvested} from "@/components";
-
+import {
+    PortfolioExposure,
+    PortfolioStructure,
+    RealPnl,
+    TotalBalance,
+    TotalInvested,
+} from "@/components";
 
 export function TopStats() {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 w-full">
-            <CardContainer>
-                <TotalBalance/>
+        <div
+            className="
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        gap-4
+        w-full
+        transition-all
+        duration-300
+      "
+        >
+            <CardContainer className="min-h-[120px] flex flex-col justify-center">
+                <TotalBalance />
             </CardContainer>
 
-            <CardContainer>
-                <TotalInvested/>
+            <CardContainer className="min-h-[120px] flex flex-col justify-center">
+                <TotalInvested />
             </CardContainer>
 
-            <CardContainer>
-                <RealPnl/>
+            <CardContainer className="min-h-[120px] flex flex-col justify-center">
+                <RealPnl />
             </CardContainer>
 
-            <CardContainer>
-                <PortfolioStructure/>
+            <CardContainer className="min-h-[120px] flex flex-col justify-center">
+                <PortfolioStructure />
             </CardContainer>
 
-            <CardContainer>
-                <PortfolioExposure/>
+            <CardContainer className="min-h-[120px] flex flex-col justify-center">
+                <PortfolioExposure />
             </CardContainer>
         </div>
     );
 }
+
