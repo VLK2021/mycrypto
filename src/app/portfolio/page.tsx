c341b85abf1a5989a6df79990e3a4a94effd4cf6
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-11-12 21:18:16 +0200

    commit

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,27 @@
+"use client"
+
+import {TopStats} from "@/components";
+import React from "react";
+import uk from "@/locales/uk";
+import en from "@/locales/en";
+import {useLanguage} from "@/context";
+
+
 export default function Portfolio() {
+    const { lang } = useLanguage();
+    const t = lang === "uk" ? uk : en;
+
+
     return (
-        <div>
-            Portfolio
+        <div className="min-h-screen text-[var(--color-text)] bg-[var(--color-background)] px-1 md:px-8 py-3 flex flex-col gap-4"
+        >
+            <h1 className="text-lg font-semibold mb-2">{"PORTFOLIO"}</h1>
+
+            <div className="w-full flex">
+                <TopStats/>
+            </div>
+
+
         </div>
     )
 }
